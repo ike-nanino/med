6e1361bccb8a27450f5126a866226a893d3868c6
ike-nanino <ikesemanu@gmail.com>
2026-05-23 03:39:02 -0400

    mistake mistake

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'
 
 import Navbar       from '@/components/Navbar'
 import Footer       from '@/components/Footer'
-import EmergencyBar from '@/components/Emergencybar'
+import EmergencyBar from '@/components/EmergencyBar'
 
 export const metadata: Metadata = {
   title: {
